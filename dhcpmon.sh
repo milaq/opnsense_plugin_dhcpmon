3221c5449dd51dcd2d07ed0d9aece5cc03b5ebd1
milaq <micha.laqua@gmail.com>
2020-02-02 16:35:04 +0100

    initial import

---
+++ dhcpmon.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# goes into /usr/local/bin/dhcpmon.sh
+
+log () {
+  echo "$1"
+  logger -t "dhcpmon" "$1"
+}
+
+if [ -z $1 ]; then
+  log "[ERROR] No interface specified"
+  exit 1
+fi
+
+IFACE=$1
+
+ifconfig $IFACE > /dev/null 2>&1
+if [ $? -gt 0 ]; then
+  log "[ERROR] Interface $IFACE does not exist"
+  exit 1
+fi
+
+curtime=$(date +%s)
+uptime=$(sysctl kern.boottime | awk -F'sec = ' '{print $2}' | awk -F',' '{print $1}')
+uptime=$(($curtime - $uptime))
+if [ $uptime -lt 300 ]; then
+  log "System uptime is less than 5m. Not checking DHCP status of $IFACE"
+  exit 0
+fi
+
+if [ $(ifconfig $IFACE | grep -c -w inet) -eq 0 ]; then
+  log "$IFACE DHCP seems down, reloading interface..."
+  ifconfig $IFACE down
+  sleep 1
+  ifconfig $IFACE up
+fi
